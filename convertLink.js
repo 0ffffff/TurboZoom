a4bde19d8e4e21191ade94f74c599aeac28b2984
Brucewastaken <68714491+Brucewastaken@users.noreply.github.com>
2022-04-09 17:56:38 -0700

    Update
    Bug fixes, improved stability

--- convertLink.js
+++ convertLink.js
@@ -1,4 +1,4 @@
-var convertButton = document.getElementById('convertButton');
+/*var convertButton = document.getElementById('convertButton');
 convertButton.addEventListener('click', function(){
   convertLink();
 });
@@ -21,18 +21,68 @@ function convertLink() {
 
   if (!clipboardContents.includes('zoom.us')) {
     console.log(clipboardContents + ' is not a valid zoom link.');
-    return alert('invalid zoom link :)');
+    return alert('invalid zoom link.');
   }
 
   var joinID = clipboardContents.split('/j/')[1];
   
   if (!joinID) {
     console.log(clipboardContents + ' is not a valid zoom link.');
-    return alert('invalid zoom link :)');
+    return alert('invalid zoom link.');
   }
 
-  joinID = "zoommtg://zoom.us/join?confno=" + joinID;
+  var password = joinID.split('pwd=')[1];
+
+  if (password) {
+    joinID = "zoommtg://zoom.us/join?action=join&confno=" + joinID.split('pwd=')[0] + '&pwd=' + password;
+  } else {
+    joinID = "zoommtg://zoom.us/join?action=join&confno=" + joinID;
+  }
+
   console.log(joinID);
   //chrome.tabs.create({ url: joinID });
   alert('the smart zoom link is: ' + joinID);
-}
+}*/
+
+var convertForm = document.getElementById('convert');
+var convUrl = document.getElementById('convUrl');
+var results = document.getElementById('resultBox');
+var zoomUrl = document.getElementById('zoomUrl');
+
+convertForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (convUrl.value) {
+    var convUrlValue = convUrl.value;
+    if (convUrlValue.includes('>' || '<')) {
+      convUrlValue = convUrlValue.split('>')[1].split('<')[0];
+    }
+
+    if (!convUrlValue.includes('zoom.us')) {
+      console.log(convUrlValue + ' is not a valid zoom link.');
+      return alert('invalid zoom link.');
+    }
+
+    var joinID = convUrlValue.split('/j/')[1];
+
+    if (!joinID) {
+      joinID = convUrlValue.split('confno=')[1];
+      if (!joinID) {
+        console.log(convUrlValue + ' is not a valid zoom link.');
+        return alert('invalid zoom link.');
+      }
+    }
+
+    var password = joinID.split('pwd=')[1];
+
+    if (password) {
+      joinID = "zoommtg://zoom.us/join?action=join&confno=" + joinID.split('pwd=')[0] + '&pwd=' + password;
+    } else {
+      joinID = "zoommtg://zoom.us/join?action=join&confno=" + joinID;
+    }
+
+    console.log(joinID);
+    //chrome.tabs.create({ url: joinID });
+    results.innerHTML = joinID;
+    zoomUrl.value = joinID;
+  }
+});
